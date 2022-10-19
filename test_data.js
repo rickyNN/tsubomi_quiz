fc2a53e775b68abe8918a4c5e36cd35d39289e06
rickyNN <ricky275832129@yahoo.co.jp>
2022-10-19 19:15:20 +0900

    龍華2年社会を追加、次回用のテストを追加

---
+++ test_data.js
@@ -0,0 +1,42 @@
+const quizDataJp = [
+  {
+    check: 'test_quiz',
+    question: '答えはA',
+    a: 'A',
+    b: 'B',
+    c: 'C',
+    d: 'D',
+    explanation: 'この問題の答えはAです',
+  },
+  {
+    check: 'test_quiz',
+    question: '答えはB',
+    a: 'B',
+    b: 'A',
+    c: 'C',
+    d: 'D',
+    explanation: 'この問題の答えはBです',
+  },
+  {
+    check: 'test_quiz',
+    question: '答えはC',
+    a: 'C',
+    b: 'A',
+    c: 'D',
+    d: 'B',
+    explanation: 'この問題の答えはCです',
+  },
+
+  //学年 科目 単元
+
+
+  ////// 中１
+
+  //// 中１ 亀井
+
+  // 中１ 亀井 英語
+
+
+];
+
+export { quizDataJp }
