1d37ab081c9ab2e6798525eff1ec6821cbb440ef
rickyNN <ricky275832129@yahoo.co.jp>
2023-06-14 18:20:47 +0900

    quizdataを変更

--- quiz_data_jp.js
+++ quiz_data_jp.js
@@ -1,29 +1,29 @@
 const quizDataJp = [{
-// 学校名(スペースで複数選択可)
-  school: 'kamei ryuge',
-// 学年
-  grade: 'grade1',
-// 教科(英語:E、数学M、理科SI,社会SS)
-  subject: 'E',
-  question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
-  a: 'a',
-  b: 'b',
-  c: 'c',
-  d: 'd',
-  explanation: 'a',
-},{
-  // 学校名(スペースで複数選択可)
-  school: 'kamei ryuge',
-// 学年
-  grade: 'grade1',
-// 教科(英語:E、数学M、理科SI,社会SS)
-  subject: 'E',
-  question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
-  a: 'a',
-  b: 'b',
-  c: 'c',
-  d: 'd',
-  explanation: 'a',
+// // 学校名(スペースで複数選択可)
+//   school: 'kamei ryuge',
+// // 学年
+//   grade: 'grade1',
+// // 教科(英語:E、数学M、理科SI,社会SS)
+//   subject: 'E',
+//   question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
+//   a: 'a',
+//   b: 'b',
+//   c: 'c',
+//   d: 'd',
+//   explanation: 'a',
+// },{
+//   // 学校名(スペースで複数選択可)
+//   school: 'kamei ryuge',
+// // 学年
+//   grade: 'grade1',
+// // 教科(英語:E、数学M、理科SI,社会SS)
+//   subject: 'E',
+//   question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
+//   a: 'a',
+//   b: 'b',
+//   c: 'c',
+//   d: 'd',
+//   explanation: 'a',
 }
 
 
