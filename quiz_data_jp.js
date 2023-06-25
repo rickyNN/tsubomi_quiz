d28de8713ef357757c26af085d9fa370efd1e347
rickyNN <ricky275832129@yahoo.co.jp>
2023-06-25 14:33:00 +0900

    亀井中3数学の問題を追加(技術の問題

--- quiz_data_jp.js
+++ quiz_data_jp.js
@@ -1,29 +1,127 @@
 const quizDataJp = [{
-// // 学校名(スペースで複数選択可)
-//   school: 'kamei ryuge',
-// // 学年
-//   grade: 'grade1',
-// // 教科(英語:E、数学M、理科SI,社会SS)
-//   subject: 'E',
-//   question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
-//   a: 'a',
-//   b: 'b',
-//   c: 'c',
-//   d: 'd',
-//   explanation: 'a',
-// },{
-//   // 学校名(スペースで複数選択可)
-//   school: 'kamei ryuge',
-// // 学年
-//   grade: 'grade1',
-// // 教科(英語:E、数学M、理科SI,社会SS)
-//   subject: 'E',
-//   question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
-//   a: 'a',
-//   b: 'b',
-//   c: 'c',
-//   d: 'd',
-//   explanation: 'a',
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '情報に関する技術によって情報が瞬時にと大量にやりとりされる社会をなんというか',
+    a: '情報社会',
+    b: 'ネットワーク社会',
+    c: 'ダイナミック社会',
+    d: '通信社会',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'web上で「社会的なつながり」を持つことができるサービスのことをアルファベット3文字でなんというか',
+    a: 'SNS',
+    b: 'AWS',
+    c: 'TPO',
+    d: 'SOS',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '現実世界の物理的なモノ(電化製品など)に通信機能を搭載して、インターネットに接続・連携させる技術をなんというか',
+    a: 'Iot',
+    b: 'ICT',
+    c: 'DbD',
+    d: 'NCT',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'インターネットなどの情報通信技術を使えない人と使える人の間に生じる格差を指す言葉をなんというか',
+    a: '情報格差',
+    b: 'アパルトヘイト',
+    c: '情報落差',
+    d: '白豪主義',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'バーコードを使って商品の販売情報を記録し、在庫管理に役立てるシステムをなんというか',
+    a: 'POSシステム',
+    b: 'Iotシステム',
+    c: 'IRDシステム',
+    d: 'PTOシステム',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '目的を達成するために、複数のものを組み合わせたものをなんというか',
+    a: 'システム',
+    b: 'ネットワーク',
+    c: 'ハードウェア',
+    d: 'ソフトウェア',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'コンピュータ本体や接続された機器のことをなんというか',
+    a: 'ハードウェア',
+    b: 'ネットワーク',
+    c: 'システム',
+    d: 'ソフトウェア',
+    explanation: ' ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'コンピュータの処理を記述したプログラムを指す言葉をなんというか',
+    a: 'ソフトウェア',
+    b: 'ネットワーク',
+    c: 'システム',
+    d: 'ハードウェア',
+    explanation: ' ',
+}, {
+    //   // 学校名(スペースで複数選択可)
+    //   school: 'kamei ryuge',
+    // // 学年
+    //   grade: 'grade1',
+    // // 教科(英語:E、数学M、理科SI,社会SS)
+    //   subject: 'E',
+    //   question: '次の文章の英訳として最も適切なものはどれ？\n私の父は高校で数学を教えています。',
+    //   a: 'a',
+    //   b: 'b',
+    //   c: 'c',
+    //   d: 'd',
+    //   explanation: 'a',
 }
 
 
