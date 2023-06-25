e0c0ec430e7df8de5a11ab2d1d36344eb34d8a2f
rickyNN <ricky275832129@yahoo.co.jp>
2023-06-25 15:41:14 +0900

    亀井中3数学の問題を追加(技術の問題

--- quiz_data_jp.js
+++ quiz_data_jp.js
@@ -2,15 +2,15 @@ const quizDataJp = [{
     // 学校名(スペースで複数選択可)
     school: 'kamei',
     // 学年
-    grade: 'grade1',
+    grade: 'grade3',
     // 教科(英語:E、数学M、理科SI,社会SS)
-    subject: 'E',
+    subject: 'M',
     question: '情報に関する技術によって情報が瞬時にと大量にやりとりされる社会をなんというか',
     a: '情報社会',
     b: 'ネットワーク社会',
     c: 'ダイナミック社会',
     d: '通信社会',
-    explanation: ' ',
+    explanation: '情報社会',
 },
 {
     // 学校名(スペースで複数選択可)
@@ -22,9 +22,9 @@ const quizDataJp = [{
     question: 'web上で「社会的なつながり」を持つことができるサービスのことをアルファベット3文字でなんというか',
     a: 'SNS',
     b: 'AWS',
-    c: 'TPO',
+    c: 'CPU',
     d: 'SOS',
-    explanation: ' ',
+    explanation: 'SNS',
 },
 {
     // 学校名(スペースで複数選択可)
@@ -38,7 +38,7 @@ const quizDataJp = [{
     b: 'ICT',
     c: 'DbD',
     d: 'NCT',
-    explanation: ' ',
+    explanation: 'Iot',
 },
 {
     // 学校名(スペースで複数選択可)
@@ -52,7 +52,7 @@ const quizDataJp = [{
     b: 'アパルトヘイト',
     c: '情報落差',
     d: '白豪主義',
-    explanation: ' ',
+    explanation: '情報格差',
 },
 {
     // 学校名(スペースで複数選択可)
@@ -63,10 +63,10 @@ const quizDataJp = [{
     subject: 'M',
     question: 'バーコードを使って商品の販売情報を記録し、在庫管理に役立てるシステムをなんというか',
     a: 'POSシステム',
-    b: 'Iotシステム',
+    b: 'CPUシステム',
     c: 'IRDシステム',
     d: 'PTOシステム',
-    explanation: ' ',
+    explanation: 'POSシステム',
 },
 {
     // 学校名(スペースで複数選択可)
@@ -80,7 +80,7 @@ const quizDataJp = [{
     b: 'ネットワーク',
     c: 'ハードウェア',
     d: 'ソフトウェア',
-    explanation: ' ',
+    explanation: 'システム',
 },{
     // 学校名(スペースで複数選択可)
     school: 'kamei',
@@ -93,7 +93,7 @@ const quizDataJp = [{
     b: 'ネットワーク',
     c: 'システム',
     d: 'ソフトウェア',
-    explanation: ' ',
+    explanation: 'ハードウェア',
 },{
     // 学校名(スペースで複数選択可)
     school: 'kamei',
@@ -106,8 +106,107 @@ const quizDataJp = [{
     b: 'ネットワーク',
     c: 'システム',
     d: 'ハードウェア',
-    explanation: ' ',
-},{
+    explanation: 'ソフトウェア',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '目的に応じて使用するソフトウェアをなんというか',
+    a: 'アプリケーションソフトウェア',
+    b: 'ネットワークソフトウェア',
+    c: 'インターネットソフトウェア',
+    d: 'オペレーションソフトウェア',
+    explanation: 'アプリケーションソフトウェア',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'アプリケーションソフトウェアの土台として働く基本ソフトウェアをなんというか',
+    a: 'オペレーションソフトウェア',
+    b: 'ネットワークソフトウェア',
+    c: 'インターネットソフトウェア',
+    d: 'アプリケーションソフトウェア',
+    explanation: 'アプリケーションソフトウェア',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '目的に応じて使用するソフトウェアをなんというか',
+    a: 'アプリケーションソフトウェア',
+    b: 'ネットワークソフトウェア',
+    c: 'インターネットソフトウェア',
+    d: 'スマートフォンソフトウェア',
+    explanation: 'アプリケーションソフトウェア',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '中央処理装置をアルファベット3文字でなんというか',
+    a: 'CPU',
+    b: 'AWS',
+    c: 'SNS',
+    d: 'SOS',
+    explanation: 'CPU',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '一時的にデータを記憶させておく部分をなんというか',
+    a: 'メモリ',
+    b: 'ストレージ',
+    c: 'GPU',
+    d: 'グラフィックボード',
+    explanation: 'メモリ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: '電源を切った状態でもデータを保存させ続ける装置をなんというか',
+    a: 'ストレージ',
+    b: 'メモリ',
+    c: 'GPU',
+    d: 'グラフィックボード',
+    explanation: 'ストレージ',
+},
+{
+    // 学校名(スペースで複数選択可)
+    school: 'kamei',
+    // 学年
+    grade: 'grade3',
+    // 教科(英語:E、数学M、理科SI,社会SS)
+    subject: 'M',
+    question: 'コンピュータグラフィックスの処理に特化した処理装置をなんというか',
+    a: 'GPU',
+    b: 'メモリ',
+    c: 'ストレージ',
+    d: 'グラフィックボード',
+    explanation: 'GPU',
+},
+{
     //   // 学校名(スペースで複数選択可)
     //   school: 'kamei ryuge',
     // // 学年
